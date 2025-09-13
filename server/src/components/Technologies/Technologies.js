0534b33c7c5fccc770f9ae7a50b4112efdbc48ff
Henry Pham <henrypham6719@gmail.com>
2025-09-13 00:17:23 -0500

    added technology section
    add technology section
    add list container
    add experience(mern, apis, godot, github, etc

--- server/src/components/Technologies/Technologies.js
+++ server/src/components/Technologies/Technologies.js
@@ -4,9 +4,45 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+    I’ve worked across a wide range of tech—from full-stack web (MERN) to game dev in Godot 4 to Discord bots. I’m always eager to learn and adapt to new tools and frameworks.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js, HTML, CSS, JavaScript
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node.js, Express.js, MongoDB
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>Other</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Git, GitHub, RESTful APIs, Godot 4
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
